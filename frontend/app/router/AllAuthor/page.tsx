983d4e26839ce74fd619504184d0e7eaabb0ac4d
ali-imtiyazkhan <itzkhantijara@gmail.com>
2025-09-20 14:21:00 +0530

    All user correct 2

--- frontend/app/router/AllAuthor/page.tsx
+++ frontend/app/router/AllAuthor/page.tsx
@@ -42,7 +42,7 @@ export default function GetAllUser() {
             }
 
             try {
-                const res = await fetch("http://localhost:8787/api/v1/user/getAllUser", {
+                const res = await fetch("https://backend.blog-backend-imtiyaz.workers.dev/api/v1/user/getAllUser", {
                     headers: {
                         "Content-Type": "application/json",
                         Authorization: `Bearer ${token}`,
